e7282350b7965b317b7fdb7bd6d87f1c2ba2dd52
Madeline Witters <mmwitters@gmail.com>
2023-05-19 15:19:55 -0500

    Importing wine dataset

--- wine.py
+++ wine.py
@@ -1,2 +1,5 @@
 import pandas as pd
 
+wine = pd.read_csv('wine.csv', names = ["Cultivator", "Alchol", "Malic_Acid", "Ash", "Alcalinity_of_Ash", "Magnesium", "Total_phenols", "Falvanoids", "Nonflavanoid_phenols", "Proanthocyanins", "Color_intensity", "Hue", "OD280", "Proline"])
+
+print(wine.head())
