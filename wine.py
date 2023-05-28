920142f0eba0572dcbcb22b4339f0a46756592ef
Madeline Witters <mmwitters@gmail.com>
2023-05-28 15:21:48 -0500

    Exploring data

--- wine.py
+++ wine.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-wine = pd.read_csv('wine.csv', names = ["Cultivator", "Alchol", "Malic_Acid", "Ash", "Alcalinity_of_Ash", "Magnesium", "Total_phenols", "Falvanoids", "Nonflavanoid_phenols", "Proanthocyanins", "Color_intensity", "Hue", "OD280", "Proline"])
+wine = pd.read_csv('wine.csv', names = ["Cultivator", "Alcohol", "Malic_Acid", "Ash", "Alcalinity_of_Ash", "Magnesium", "Total_phenols", "Falvanoids", "Nonflavanoid_phenols", "Proanthocyanins", "Color_intensity", "Hue", "OD280", "Proline"])
 
 print(wine.head())
+
+print(wine['Alcohol'].head(10))
