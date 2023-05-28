7ceae6bad9b176f656b37084805589f788c9da27
Madeline Witters <mmwitters@gmail.com>
2023-05-28 15:42:13 -0500

    Imported matplotlib and added visualizations

--- wine.py
+++ wine.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 wine = pd.read_csv('wine.csv', names = ["Cultivator", "Alcohol", "Malic_Acid", "Ash", "Alcalinity_of_Ash", "Magnesium", "Total_phenols", "Falvanoids", "Nonflavanoid_phenols", "Proanthocyanins", "Color_intensity", "Hue", "OD280", "Proline"])
 
 print(wine.head())
 
 print(wine['Alcohol'].head(10))
+
+plt.plot(wine['Malic_Acid'])
+plt.show()
